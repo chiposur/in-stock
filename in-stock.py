df01cf1f939217e74aa5bb1707fc38dccd2d937e
chiposur <osurov314@gmail.com>
2022-11-29 15:22:55 -0800

    Adding requests dependency, fetching document and some error handling

--- in-stock.py
+++ in-stock.py
@@ -1,3 +1,4 @@
+import requests
 from time import sleep
 
 class InStock:
@@ -11,6 +12,8 @@ class InStock:
     try:
       print("Enter URL:")
       self.url = str(input())
+      if not self.url:
+        raise Exception("URL is required")
       print("Enter selector:")
       self.selector = str(input())
       print("Enter cooldown time in ms:")
@@ -27,9 +30,23 @@ class InStock:
     while self.running:
       self.checkAvailability()
       sleep(self.cooldownMs / 1000)
+    self.notify()
 
   def checkAvailability(self):
     print("Checking availability...")
+    print(f"Retrieving '{self.url}'...")
+    try:
+      req = requests.get(self.url, timeout=(8, 30))
+    except requests.exceptions.ReadTimeout as e:
+      print(f'Timeout reading request...')
+      return
+    if req.status_code == 200:
+      print("Request successful, parsing...")
+    else:
+      print(f'Request failed with error code {req.status_code}')
+
+  def notify(self):
+    pass
 
 if __name__ == "__main__":
     inStock = InStock()
